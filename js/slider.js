ab140c503fedc9c14f2e0bdb7dfe4f599d0b51f7
Abigail Nguyen <abigail.nguyen@papercut.com>
2019-09-15 08:21:43 +1000

    website

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,9 @@
 $("#signUpBtn").click(function() {
     $(".panel-wrap").css("transform", "translateX(0%)");
+    $(".overlay-content").css("display", "block");
+});
+
+$("button.close").click(function() {
+    $(".panel-wrap").css("transform", "translateX(100%)");
+    $(".overlay-content").css("display", "none");
 });
